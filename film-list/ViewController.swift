a4e38ebd09e78c667e3523df23bd7df6978e04e7
Randy Efan <randy@mamiteam.com>
2021-04-17 18:46:34 +0900

    Initial Commit

---
+++ film-list/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  film-list
+//
+//  Created by Randy Efan Jayaputra on 17/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
