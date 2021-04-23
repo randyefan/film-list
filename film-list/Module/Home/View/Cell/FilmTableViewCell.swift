0b4eefac86b73b3a35477685034f411f9cbd3e78
Randy Efan Jayaputra <randyrandyrej8@gmail.com>
2021-04-23 19:48:12 +0900

    edit cell class

--- film-list/Module/Home/View/Cell/FilmTableViewCell.swift
+++ film-list/Module/Home/View/Cell/FilmTableViewCell.swift
@@ -27,9 +27,4 @@ class FilmTableViewCell: UITableViewCell {
     override func awakeFromNib() {
         super.awakeFromNib()
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-    }
-    
 }
